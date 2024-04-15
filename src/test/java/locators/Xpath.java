09178dd685bb5115674c942568e419d6efeeab0b
Vaibhav <Vaibhav@Vaibhav-HP>
2024-04-15 23:18:18 +0530

    15-04

--- src/test/java/locators/Xpath.java
+++ src/test/java/locators/Xpath.java
@@ -10,17 +10,35 @@ public class Xpath
 	public static void main(String[] args)
 	{
 		WebDriver ob = new ChromeDriver();
-		ob.get("https://demo.nopcommerce.com/");
+		ob.get("https://demo.opencart.com/");
 		// xpath with single attribute
-		ob.findElement(By.xpath("//input[@autocomplete='off']")).sendKeys("apple");
-		// xpath with multiple attribute
-		ob.findElement(By.xpath("//input[@autocomplete='off'][@id='small-searchterms']"));
+//		ob.findElement(By.xpath("//input[@autocomplete='off']")).sendKeys("apple");
+//		// xpath with multiple attribute
+//		ob.findElement(By.xpath("//input[@autocomplete='off'][@id='small-searchterms']"));
+//
+//		// xpath with 'and' operator
+//		ob.findElement(By.xpath("//input[@autocomplete='off' and @id='small-searchterms']"));
+//
+//		// xpath with 'and' operator
+//		ob.findElement(By.xpath("//input[@autocomplete='off' or @id='small-searchterms']"));
+//		
+//		//Xpath with inner text- text()
+//		ob.findElement(By.xpath("//a[text()='Desktops']"));
+//		
+		Boolean display = ob.findElement(By.xpath("//h3[text()='Featured']")).isDisplayed();
+		System.out.println(display);
 
-		// xpath with 'and' operator
-		ob.findElement(By.xpath("//input[@autocomplete='off' and @id='small-searchterms']"));
+		String value = ob.findElement(By.xpath("//h3[text()='Featured']")).getText();
+		System.out.println(value);
 
-		// xpath with 'and' operator
-		ob.findElement(By.xpath("//input[@autocomplete='off' or @id='small-searchterms']"));
+		if (value.equals("Featured"))
+		{
+			System.out.println("value is correct");
+		}
+		else
+		{
+			System.out.println("value is not correct");
+		}
 
 	}
 
