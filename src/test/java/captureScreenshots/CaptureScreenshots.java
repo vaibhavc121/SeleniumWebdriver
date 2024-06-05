f97b8f686211207b27eb022f54e82e79e2538e30
Vaibhav <Vaibhav@Vaibhav-HP>
2024-06-05 23:43:30 +0530

    5-6

--- src/test/java/captureScreenshots/CaptureScreenshots.java
+++ src/test/java/captureScreenshots/CaptureScreenshots.java
@@ -1,10 +1,12 @@
 package captureScreenshots;
 
+import java.io.File;
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CaptureScreenshots
@@ -18,10 +20,20 @@ public class CaptureScreenshots
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
 		driver.get("https://testautomationpractice.blogspot.com/");
-		
-		//full page screenshot
-		TakesScreenshot ts = (TakesScreenshot) driver;
-		ts.getScreenshotAs(OutputType.FILE)
+
+		// full page screenshot
+		/*
+		 * TakesScreenshot ts = (TakesScreenshot) driver; File sourceFile =
+		 * ts.getScreenshotAs(OutputType.FILE); File targFile = new
+		 * File(System.getProperty("user.dir") + "\\screenshots\\fullpage.png");
+		 * sourceFile.renameTo(targFile); // copy source file to target file
+		 */
+		// Capture the screenshot of specific section
+		WebElement table = driver.findElement(By.cssSelector("#HTML1"));
+		File sourceFile = table.getScreenshotAs(OutputType.FILE);
+		File targetFile = new File(System.getProperty("user.dir") + "\\screenshots\\table.png");
+		sourceFile.renameTo(targetFile); // copy source file to target file
+
 	}
 
 }
