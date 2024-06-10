b8c23bb4d45141187eb6398842669022483de123
Vaibhav <Vaibhav@Vaibhav-HP>
2024-06-10 23:56:09 +0530

    10-6

--- src/test/java/brokenLinks/BrokenLinks.java
+++ src/test/java/brokenLinks/BrokenLinks.java
@@ -1,5 +1,7 @@
 package brokenLinks;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.Duration;
 import java.util.List;
 
@@ -18,8 +20,44 @@ public class BrokenLinks
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
 		driver.get("http://www.deadlinkcity.com/");
+		// capture all the links from website
 		List<WebElement> links = driver.findElements(By.tagName("a"));
 		System.out.println(links.size());
+		int noofbrokenlinks = 0;
+
+		for (WebElement link : links)
+		{
+			String hrefvalue = link.getAttribute("href");
+			if (hrefvalue == null || hrefvalue.isEmpty())
+			{
+				System.out.println("value is null or empty so not possible to check");
+				continue;
+			}
+
+			// hit url to the server
+			try
+			{
+
+				URL linkUrl = new URL(hrefvalue); // converted href value from string to url format
+				HttpURLConnection conn = (HttpURLConnection) linkUrl.openConnection(); // open connection to the server
+				conn.connect(); // connect to the server and send request to the server
+				if (conn.getResponseCode() >= 400)
+				{
+					System.out.println(hrefvalue + "broken link");
+					noofbrokenlinks++;
+				}
+				else
+				{
+					System.out.println(hrefvalue + "not a broken link");
+				}
+			}
+			catch (Exception e)
+			{
+				// TODO: handle exception
+			}
+
+		}
+		System.out.println("no of broken links:" + noofbrokenlinks);
 	}
 
 }
