055915a38e32140be5ddb844c9f55b8712d2f3a8
Vaibhav <Vaibhav@Vaibhav-HP>
2024-07-09 00:17:09 +0530

    9-7

--- src/test/java/testNG/DataProviderDemo.java
+++ src/test/java/testNG/DataProviderDemo.java
@@ -2,8 +2,10 @@ package testNG;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
@@ -20,11 +22,18 @@ public class DataProviderDemo
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
 
-	@Test
-	void testlogin()
+	@Test(dataProvider = "dp")
+	void testlogin(String email, String pwd) throws InterruptedException // according to type of data provide the data
+																			// type of parameters
 	{
-		driver.get("https://demo.nopcommerce.com/");
+		driver.get("https://tutorialsninja.com/demo/index.php?route=account/login");
 		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//input[@id='input-email']")).sendKeys(email);
+		driver.findElement(By.xpath("//input[@id='input-password']")).sendKeys(pwd);
+		driver.findElement(By.xpath("//input[@value='Login']")).click();
+		Thread.sleep(2000);
+		Assert.assertTrue(false);
+
 	}
 
 	@AfterClass
@@ -33,7 +42,8 @@ public class DataProviderDemo
 
 	}
 
-	@DataProvider
+	@DataProvider(name = "dp", indices =
+	{ 0, 4 })
 	Object[][] logindata()
 	{
 		Object data[][] =
---
+++ src/test/java/testNG/ParallelTesting.java
@@ -0,0 +1,72 @@
+package testNG;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class ParallelTesting
+{
+	WebDriver driver;
+
+	@BeforeClass
+	@Parameters(
+	{ "browser", "url" })
+	void setup(String br, String url) throws InterruptedException
+	{
+		switch (br.toLowerCase())
+		{
+		case "chrome":
+			driver = new ChromeDriver();
+			break;
+		case "edge":
+			driver = new EdgeDriver();
+			break;
+
+		default:
+			System.out.println("invalid browser");
+			return; // if parameter is invalid then return command will exit from method and rest of
+					// the code will not be executed
+
+		}
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get(url);
+		Thread.sleep(2000);
+	}
+
+	@Test(priority = 1)
+	void testLogo()
+	{
+		boolean status = driver.findElement(By.xpath("//img[@alt='company-branding']")).isDisplayed();
+		Assert.assertEquals(status, true);
+	}
+
+	@Test(priority = 2)
+	void testTitle()
+	{
+		Assert.assertEquals(driver.getTitle(), "OrangeHRM");
+	}
+
+	@Test(priority = 3)
+	void testURL()
+	{
+		Assert.assertEquals(driver.getCurrentUrl(),
+				"https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+	}
+
+	@AfterClass
+	void tearDown()
+	{
+		driver.quit();
+	}
+
+}
