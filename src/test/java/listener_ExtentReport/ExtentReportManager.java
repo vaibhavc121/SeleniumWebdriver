dc904bc9bf8cf7f52a70b642e0eb1d77af445a40
vaibhavc121 <vaibhavc121@gmail.com>
2024-07-14 22:55:45 +0530

    14-7

--- src/test/java/listener_ExtentReport/ExtentReportManager.java
+++ src/test/java/listener_ExtentReport/ExtentReportManager.java
@@ -1,8 +1,68 @@
 package listener_ExtentReport;
 
+import org.testng.ITestContext;
 import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class ExtentReportManager implements ITestListener
 {
+	public ExtentSparkReporter sparkReporter; // ui of the report
+	public ExtentReports extent; // populate common info on the report
+	public ExtentTest test; // creating test case entries in the report and update status of the test
+							// methods
+
+	public void onStart(ITestContext context)
+	{
+		sparkReporter = new ExtentSparkReporter(System.getProperty("user.dir") + "/reports/extentReport.html"); // specify
+																												// the
+																												// folder
+																												// location
+		// report name should be generate dynamically
+
+		sparkReporter.config().setDocumentTitle("Automation report"); // title of the report
+		sparkReporter.config().setReportName("functional testing"); // name of the report
+		sparkReporter.config().setTheme(Theme.DARK);
+
+		extent = new ExtentReports();
+		extent.attachReporter(sparkReporter);
+
+		// values should be fetch at runtime
+		extent.setSystemInfo("computer name", "localhost");
+		extent.setSystemInfo("env", "QA");
+		extent.setSystemInfo("tester name", "vaibhav");
+		extent.setSystemInfo("os", "win11");
+		extent.setSystemInfo("browser name", "chrome");
+	}
+
+	public void onTestSuccess(ITestResult result)
+	{
+		test = extent.createTest(result.getName()); // create new entry in the report
+		test.log(Status.PASS, "test case passed is: " + result.getName()); // update status p/f/s
+	}
+
+	public void onTestFailure(ITestResult result)
+	{
+		test = extent.createTest(result.getName());
+		test.log(Status.FAIL, "test case failed is: " + result.getName());
+		test.log(Status.FAIL, "test case failed cause is: " + result.getThrowable());
+		// ss attachment in report is missing here
+	}
+
+	public void onTestSkipped(ITestResult result)
+	{
+		test = extent.createTest(result.getName());
+		test.log(Status.SKIP, "test case skipped is: " + result.getName());
+	}
+
+	public void onFinish(ITestContext context) // mandatory method
+	{
+		extent.flush();
+	}
 
 }
---
+++ src/test/java/listener_ExtentReport/ListenerTest.java
@@ -0,0 +1,56 @@
+package listener_ExtentReport;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+//@Listeners(listener_ExtentReport.ExtentReportManager.class) // add this annotation if u don't want to execute from xml
+// file
+public class ListenerTest
+{
+	WebDriver driver;
+
+	@BeforeClass
+	void setup()
+	{
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+	}
+
+	@Test(priority = 1)
+	void testLogo() throws InterruptedException
+	{
+		Thread.sleep(3000);
+		Boolean status = driver.findElement(By.xpath("//img[@alt='company-branding']")).isDisplayed();
+		// System.out.println(status);
+		Assert.assertTrue(true);
+	}
+
+	@Test(priority = 2)
+	void testAppURL()
+	{
+		Assert.assertEquals(driver.getCurrentUrl(), "dummy");
+	}
+
+	@Test(priority = 3, dependsOnMethods =
+	{ "testAppURL" })
+	void testHomePageTitle()
+	{
+		Assert.assertEquals(driver.getTitle(), "OrangeHRM");
+	}
+
+	@AfterClass
+	void tearDown()
+	{
+		// driver.quit();
+	}
+}
